02ab39c88cdecab95e239865c30804b0ac70700d
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-09-30 13:22:40 -0400

    Create KnightSolver module

---
+++ lib/knight_solver.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'set'
+
+# This module stores the methods necessary for determining the shortest path a knight
+# chesspiece can take to get from any one square to another on a chessboard.
+module KnightSolver
+  extend self # rubocop:disable Style/ModuleFunction
+
+  def knight_moves(start_pos, end_pos); end
+end
